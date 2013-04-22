118a0b4ed413c942b78895ce763867fb2c531d78
Quinn Slack <qslack@qslack.com>
2013-04-22 02:16:08 -0700

    Basic HTTP server

---
+++ app.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "RequestURI: %s\n", r.RequestURI)
+	fmt.Fprintf(w, "User-Agent: %s\n", r.Header.Get("User-Agent"))
+}
+
+func main() {
+	var addr = flag.String("http", ":8080", "HTTP service address")
+	flag.Parse()
+
+	http.HandleFunc("/", handler)
+	log.Println("Starting HTTP server on", *addr)
+	err := http.ListenAndServe(*addr, nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
